56757fd25463c542591cb369408f7eb1f7d2db55
Florian <florianlegal@outlook.fr>
2024-11-21 16:55:13 +0100

    User being 18 is now not considered as minor anymore

--- python/shop.py
+++ python/shop.py
@@ -22,12 +22,7 @@ class User:
 class Shop:
     @classmethod
     def can_order(cls, user):
-        if user.age <= 18:
-            return False
-        if not user.verified:
-            return False  # Was True instead of False.
-        else:
-            return True
+        return user.age >= 18 and user.verified
 
     @classmethod
     def must_pay_foreign_fee(cls, user):
